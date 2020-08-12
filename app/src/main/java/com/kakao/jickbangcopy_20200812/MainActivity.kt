9092ea1c492f5b9129af596120421d26006ff7ba
jang <dkjang@naver.com>
2020-08-12 21:58:17 +0900

    데이터초기화

--- app/src/main/java/com/kakao/jickbangcopy_20200812/MainActivity.kt
+++ app/src/main/java/com/kakao/jickbangcopy_20200812/MainActivity.kt
@@ -2,12 +2,19 @@ package com.kakao.jickbangcopy_20200812
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import com.kakao.jickbangcopy_20200812.adapters.RoomAdapter
 import com.kakao.jickbangcopy_20200812.datas.Room
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
 
-//    STEP5.ArrayList 만들기
+    //    STEP5.ArrayList 만들기
     val mRoomList = ArrayList<Room>()
+
+    //    STEP6.RoomAdapter 만들기
+    lateinit var mRoomAdapter: RoomAdapter
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,5 +44,10 @@ class MainActivity : BaseActivity() {
         mRoomList.add(Room(38000, "서울시 동대문구", 2, "동대문구 방입니다."))
         mRoomList.add(Room(57000, "서울시 동대문구", -2, "동대문구 방입니다."))
         mRoomList.add(Room(85000, "서울시 동대문구", 5, "동대문구 방입니다."))
+
+//        데이터초기화
+        mRoomAdapter = RoomAdapter(mContext, R.layout.room_list_item, mRoomList)
+        roomListView.adapter = mRoomAdapter
+
     }
 }
