74ffaca6f59ceb538022c45e4a165d4f9bf40389
jang <dkjang@naver.com>
2020-08-13 21:43:19 +0900

    Room.kt 주석완료

--- app/src/main/java/com/kakao/jickbangcopy_20200812/datas/Room.kt
+++ app/src/main/java/com/kakao/jickbangcopy_20200812/datas/Room.kt
@@ -6,6 +6,7 @@ import java.util.*
 
 //com.kakao.jickbangcopy_20200812.datas
 // STEP2.Room(Kotlin Class)멤버변수 만들기
+// 생성자(가격,주소,층수,설명), Serializable(Room putExtra,getExtra)
 class Room(
     val price: Int,
     val address: String,
@@ -13,17 +14,22 @@ class Room(
     val description: String
 ) : Serializable {
 
+//    가격(int -> String) 만들어 반환하는 기능
     fun getFormattedPrice(): String {
+//    1억이 넘을경우
         if (this.price >= 10000) {
+//            억단위
             val hm = this.price / 10000 //정수계산은 정수
             val thousand = this.price % 10000
             val thousandStr = NumberFormat.getNumberInstance().format(thousand)
             return "${hm}억 ${thousandStr}"
         } else {
+//            7500 -> 7,500
             return NumberFormat.getNumberInstance().format(this.price)
         }
     }
 
+//    층수(int -> String) 만들어 반환하는 기능
     fun getFormattedFloor(): String{
         val floorStr: String
         if (this.floor >= 1) {
